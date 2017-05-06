ff3ad64c19e8617211ae1b81738b5c2fac72bf52
Santiago Lopez <sldarriulat@gmail.com>
2017-05-06 13:43:00 -0300

    Change action

--- aplika-ganados/src/main/java/com/aplika/ganados/HomeActivity.java
+++ aplika-ganados/src/main/java/com/aplika/ganados/HomeActivity.java
@@ -28,7 +28,7 @@ public class HomeActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Accion", null).show();
             }
         });
 
